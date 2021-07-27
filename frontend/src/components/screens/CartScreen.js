4c0967121ed35d9f08d1aaf21054dd06f3caad18
nortonlui <nortonlui@gmail.com>
2021-07-27 10:51:06 -0300

    Frontend user authentication and profile

--- frontend/src/components/screens/CartScreen.js
+++ frontend/src/components/screens/CartScreen.js
@@ -21,7 +21,6 @@ const CartScreen = (props) => {
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  console.log(cartItems);
 
   React.useEffect(() => {
     if (productId) {
